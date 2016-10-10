176fccd675054f7b023187e60985814767c6ba50
vagrant <jz762@cornell.edu>
2016-10-10 06:47:00 +0000

    update utc-time.py return str

--- utc-time.py
+++ utc-time.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def milliseconds():
     time_now = datetime.datetime.utcnow()
-    return (time_now - datetime.datetime(1970, 1, 1)).total_seconds()*1000
+    return str((time_now - datetime.datetime(1970, 1, 1)).total_seconds()*1000)
 
 if __name__ == '__main__':
     app.run()
