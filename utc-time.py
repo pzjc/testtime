6ccc877b27cb25175cde64dc0df8dde69a5c0285
vagrant <jz762@cornell.edu>
2016-10-10 21:31:59 +0000

    update utc-tiem to test

--- utc-time.py
+++ utc-time.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def milliseconds():
-    time_now = datetime.utcnow()
-    second = ((time_now - datetime(1970, 1, 1)).total_seconds()) * 1000
-    millisecond = '{:.2f}'.format(millisecond)
-    millisecond = str(millisecond)
+   # time_now = datetime.utcnow()
+   # second = ((time_now - datetime(1970, 1, 1)).total_seconds()) * 1000
+   # millisecond = '{:.2f}'.format(millisecond)
+   # millisecond = str(millisecond)
     #print millisecond
     #return type(time_delta)
-    return millisecond 
-
+   # return millisecond 
+    return "ok!"
 if __name__ == "__main__":
     app.run()
