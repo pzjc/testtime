44b4ae6294aee27fbb046aeea271e1d7f871df84
vagrant <jz762@cornell.edu>
2016-10-10 03:46:51 +0000

    add utc-time.py

--- utc-time.py
+++ utc-time.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 def milliseconds():
     time_now = datetime.datetime.utcnow()
     return (time_now - datetime.datetime(1970, 1, 1)).total_seconds()*1000
+
+if __name__ == '__main__':
+    app.run()
